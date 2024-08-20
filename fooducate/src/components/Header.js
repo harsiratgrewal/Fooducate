56a1ab0cfd5c443bfdace7f3cf98d33b5761abf6
aoffer123 <aoffer@oakland.edu>
2024-08-20 16:19:33 -0400

    fix: removed search bar from header

--- fooducate/src/components/Header.js
+++ fooducate/src/components/Header.js
@@ -3,9 +3,6 @@ import { auth, db, logout } from "../firebase/firebase";
 import { query, collection, getDocs, where } from "firebase/firestore";
 import LogoutIcon from '@mui/icons-material/Logout';
 import IconButton from '@mui/material/IconButton';
-import InputBase from '@mui/material/InputBase';
-import SearchIcon from '@mui/icons-material/Search';
-import { styled } from '@mui/material/styles';
 import { Box, Button, CircularProgress, Typography } from '@mui/material';
 import { useAuthState } from "react-firebase-hooks/auth";
 import AssistantIcon from '@mui/icons-material/Assistant';
@@ -34,47 +31,6 @@ function Header() {
       alert("An error occurred while logging out");
     }
   };
-  const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: 10,
-    backgroundColor: 'rgba(11, 38, 136, 0.07)',
-    color: 'rgba(14, 17, 21, 0.50)',
-    '&:hover': {
-      backgroundColor: "#E7E9F3",
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(1),
-      width: 'auto',
-    },
-  }));
-
-  const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }));
-
-  const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: '#040505',
-    width: '100%',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      [theme.breakpoints.up('sm')]: {
-        width: '16ch',
-        '&:focus': {
-          width: '24ch',
-        },
-      },
-    },
-  }));
 
   useEffect(() => {
     const fetchUserName = async () => {
@@ -103,15 +59,6 @@ function Header() {
       <Box sx={{ display: 'flex', flexDirection: 'row', marginLeft: 1, justifyContent: 'space-between', alignItems: 'center' }}>
         <Typography variant="h5" color="#232530" sx={{ width: '75%' }}>Welcome back, {name} </Typography>
         <div className="d-flex flex-row w-100 me-2 justify-content-end">
-          <Search className="me-2">
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
           <Button disableElevation
             variant="contained"
             size="medium"
